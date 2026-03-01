ec187d0afb468a62b3493eb363c7b051b59871be
野村直也 <nomuranaoya@nnMacBook-Air.local>
2026-03-01 22:30:59 +0900

    Switch to new GA ID G-BDZHQBBZRW for otsu-japan.com only

--- app/layout.tsx
+++ app/layout.tsx
@@ -58,7 +58,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <Script 
-          src="https://www.googletagmanager.com/gtag/js?id=G-B7T18E1YDX"
+          src="https://www.googletagmanager.com/gtag/js?id=G-BDZHQBBZRW"
           strategy="afterInteractive"
         />
         <Script id="google-analytics" strategy="afterInteractive">
@@ -66,7 +66,7 @@ export default function RootLayout({
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'G-B7T18E1YDX');
+            gtag('config', 'G-BDZHQBBZRW');
           `}
         </Script>
       </head>
